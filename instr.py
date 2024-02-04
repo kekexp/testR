6bc6467531b4bafb72ce880c7426c32e8d8a4872
Angeti1290 <150566310+Angeti1290@users.noreply.github.com>
2024-02-04 14:56:25 +1000

    ghfhf

--- instr.py
+++ instr.py
@@ -1,26 +1,12 @@
-
-win_x, win_y = 200, 100
-win_width, win_height = 1000, 600
-
-txt_hello = ''
-txt_next = ''
-txt_instruction = '' 
-txt_title = ''
-txt_name = ''
-txt_hintname = ""
-txt_hintage = ""
-txt_test1 = ''
-txt_test2 = ''
-txt_test3 = ''
-txt_sendresults = ''
-txt_hinttest1 = '0'
-txt_hinttest2 = '0'
-txt_hinttest3 = '0'
-txt_starttest1 = ''
-txt_starttest2 = ''
-txt_starttest3 = ''
-
-txt_age = ''
-txt_finalwin = ''
-txt_index = ''
-txt_workheart = ''
+W = 1620
+H = 780
+Startxt 'Начать'
+Title1 = 'Здоровье'
+Title2 = 'Здоровье'
+Title3 = 'Результаты'
+Welctxt = 'ijnhuibu'
+Txtest = 'Данное приложение позволит вам с помощью теста Руфье провести первичную диагностику вашего здоровья.\n Проба Руфье представляет собой нагрузочный комплекс, предназначенный для оценки работоспособности сердца при физической нагрузке.\n У испытуемого, находящегося в положении лёжа на спине в течении 5 мин, определяют частоту пульса за 15 сек, \n затем в течении 45 секунд испытуемый выполнит 30 приседаний...'
+Texting = 'Лягте на спину и замерьте пульс за 15 секунд. Нажмите кнопку "Начать пробный тест"...'
+Time = '00:15'
+Fin = ''
+ind = ''
