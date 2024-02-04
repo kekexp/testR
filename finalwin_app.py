ef925d2cd8efdbc13c407b11eb5ffe5cf343e5e7
Angeti1290 <150566310+Angeti1290@users.noreply.github.com>
2024-02-04 14:40:22 +1000

    sfdgs
    sdgs

---
+++ finalwin_app.py
@@ -0,0 +1,25 @@
+# напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt 
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit 
+from inst import*
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.appear()
+        self.initUI()
+        self.connect
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(Title3)
+        self.resize(W, H)
+
+    def initUI(self):
+        text = QLable('Индекс Руфье:', Fin)
+        text2 = QLable('Работоспособностье сердца:', ind)
+
+        line = QVBoxLayout()
+
+        line.addWidget(text, alignment = Qt.AlignCenter)
+        line.addWidget(winner, alignment = Qt.AlignCenter)
+    def connects(self):
+        pass
